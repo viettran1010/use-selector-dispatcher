05675686979c69517594cb59f3b82362e49d9ee8
Victor <victor@Victors-MacBook-Pro.local>
2021-07-25 17:16:51 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useSelector, useDispatch } from 'react-redux'
+
 function App() {
+
+  const counter = useSelector(state=>state.counter)
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{counter}</h1>
+        <button onClick={()=> dispatch({type:'INCREMENT'})}>Add</button>
       </header>
     </div>
   );
